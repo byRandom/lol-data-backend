0cc614e9b1d80405a129cac578733e0b4a366dd3
byRandom <tomasperezv11@gmail.com>
2022-05-04 18:43:00 +0200

    CORS

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ let app = require('./app')
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
-    console.log('Servidor corriendo correctamente en la url 127.0.0.1:443')
+    console.log('Servidor corriendo correctamente en la url 127.0.0.1:' + PORT)
 })

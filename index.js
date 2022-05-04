2be801fbb09593c7de8f6668789a5d44180ee4e0
byRandom <tomasperezv11@gmail.com>
2022-05-04 17:51:41 +0200

    Port Change

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict'
 let app = require('./app')
 
-let port = 3700
+let port = 443
 
 app.listen(port, ()=>{
-    console.log('Servidor corriendo correctamente en la url 127.0.0.1:3700')
+    console.log('Servidor corriendo correctamente en la url 127.0.0.1:443')
 })
